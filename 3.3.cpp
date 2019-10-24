ca08b5d4ca3d5d86d032f6a5f1b70c8c6e7de03f
Serega371 <55870079+Serega371@users.noreply.github.com>
2019-10-24 23:08:11 +0300

    Заменил названия!

---
+++ 3.3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+double km, litr;
+cout << "km=_";
+cin>>km;
+cout << "litr=";
+cin>>litr;
+cout <<"Result="<< litr/km/100<< endl;
+return 0;
+}
